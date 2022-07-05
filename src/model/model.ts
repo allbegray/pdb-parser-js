0a9a6e20c6f33dc15a5b189da25fd35c7c897ed7
allbegray <allbegray@gmail.com>
2022-07-05 14:23:09 +0900

    model 추가

--- src/model/model.ts
+++ src/model/model.ts
@@ -29,3 +29,62 @@ export interface Atom {
 
 export interface Hetatm extends Atom {
 }
+
+// not yet
+
+export interface Ssbond {
+    serNum: number | null
+    chainID1: string | null
+    seqNum1: number | null
+    icode1: string | null
+    chainID2: string | null
+    seqNum2: number | null
+    icode2: string | null
+    sym1: string | null
+    sym2: string | null
+    Length: number | null
+}
+
+export interface Het {
+    hetID: string | null
+    ChainID: string | null
+    seqNum: number | null
+    iCode: string | null
+    numHetAtoms: number | null
+    text: string | null
+}
+
+export interface Hetnam {
+    hetID: string | null
+    text: string | null
+}
+
+export interface Modres {
+    idCode: string | null
+    resName: string | null
+    chainID: string | null
+    seqNum: number | null
+    iCode: string | null
+    stdRes: string | null
+    comment: string | null
+}
+
+export interface Seqadv {
+    idCode: string | null
+    resName: string | null
+    chainID: string | null
+    seqNum: number | null
+    iCode: string | null
+    database: string | null
+    dbAccession: string | null
+    dbRes: string | null
+    dbSeq: number | null
+    conflict: string | null
+}
+
+export interface Seqres {
+    serNum: string | null
+    chainID: string | null
+    numRes: number | null
+    resNames: string[]
+}
