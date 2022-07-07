d26590c0eb4dcf33761b1eff7a466f0184005764
allbegray <allbegray@gmail.com>
2022-07-07 18:01:55 +0900

    doc

--- src/section/secondaryStructure.ts
+++ src/section/secondaryStructure.ts
@@ -1,12 +1,16 @@
 import '../extension/string';
 import {AbstractParser} from "../parser";
 
-// TODO
+/***
+ * https://www.wwpdb.org/documentation/file-format-content/format33/sect5.html#HELIX
+ */
 export interface Helix {
-
+// TODO
 }
 
-// TODO
+/***
+ * https://www.wwpdb.org/documentation/file-format-content/format33/sect5.html#SHEET
+ */
 export interface Sheet {
-
+// TODO
 }
