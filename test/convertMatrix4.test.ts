fd2ce0c036a00e8cc292d1d406f3c90f4a991390
allbegray <allbegray@gmail.com>
2022-07-15 14:19:23 +0900

    :bug: 컴파일 오류 수정

--- test/convertMatrix4.test.ts
+++ test/convertMatrix4.test.ts
@@ -4,7 +4,7 @@ import {Origx} from "../src/section/crystallographicAndCoordinateTransformation"
 
 test('toMatrix4 컨버팅 테스트', () => {
 
-    const items: Origx[] = [
+    const items: [Origx, Origx, Origx] = [
         new Origx('Orig1', 2, 0, 0, 0),
         new Origx('Orig2', 0, 3, 0, 0),
         new Origx('Orig3', 0, 0, 4, 0),
